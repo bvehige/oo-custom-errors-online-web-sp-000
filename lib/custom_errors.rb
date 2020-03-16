5a58c0b7a3b9ae98663a06fcee4e1cf6d0671279
Brian Vehige <brianvehige@gmail.com>
2020-03-16 00:59:39 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,9 +6,15 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
+    self.partner = person 
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
   end
+
+class PartnerError < StandardError
+end
 
 end
 
